69ff3dc921cdb92df01eaf8ac71b5e63490cc2f9
Andrey Mokhov <andrey.mokhov@gmail.com>
2014-12-23 00:12:41 +0000

    Add generation of results into build.

--- build.hs
+++ build.hs
@@ -3,9 +3,12 @@ import Development.Shake.FilePath
 
 target = "fibConcatPrimes" <.> exe
 primes = "Primes" </> "Primes.hs"
+results = "fibConcatPrimes.txt"
+reverseResults = "reverseFibConcatPrimes.txt"
+
 
 main = shakeArgs shakeOptions{shakeFiles="_build/"} $ do
-    want [target]
+    want [target, results, reverseResults]
 
     "clean" ~> do
         putNormal "Cleaning files in _build"
@@ -16,3 +19,12 @@ main = shakeArgs shakeOptions{shakeFiles="_build/"} $ do
         need [src, primes]
         cmd "ghc" ["-O2", "-i" ++ takeDirectory primes, src]
 
+    results %> \out -> do
+        need [target]
+        Stdout res <- cmd target
+        writeFileChanged out res
+
+    reverseResults %> \out -> do
+        need [target]
+        Stdout res <- cmd target ["-reverse"]
+        writeFileChanged out res
